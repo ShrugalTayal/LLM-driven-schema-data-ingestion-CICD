49a8d01382db1b722b50030b9924fd988483c28d
ShrugalTayal <shrugaltayal@gmail.com>
2025-08-31 18:26:23 +0530

    Docker (Postgres) & init SQL

---
+++ scripts/run_all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Exit immediately if a command fails
+set -e
+
+# Activate virtual environment
+echo "🔄 Activating virtual environment..."
+#source .venv/bin/activate
+
+# Install dependencies
+#echo "📦 Installing dependencies..."
+#pip install --upgrade pip
+#pip install -r requirements.txt
